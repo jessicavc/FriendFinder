32d6498534355464f9bc388a885e125e22439829
Jessica VC <jessicavc@this-guy-ii.home>
2020-07-11 15:18:31 -0700

    fixed issue with vulnerability. recoded server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var path = require ("path");
+var path = require("path");
 
 var app = express();
-var port = process.env.PORT || 3000; 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+var PORT = process.env.PORT || 8080;
 
-app.use(express.static("app/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-app.listen(port, () => console.log("Listening on port %s", port));
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
